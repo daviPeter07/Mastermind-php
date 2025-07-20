cb14641b1fd37b0c0169f345af738b196026c101
daviPeter07 <davipetersondev173@gmail.com>
2025-07-20 19:21:36 -0400

    feat: add connection com banco

---
+++ src/Core/Database.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+  private static ?PDO $instance = null;
+
+  private static function getConnection(): PDO {
+    if (self::$instance === null) {
+      $host = "postgres";
+      $port = getenv('DB_PORT') ?: 5432;
+      $db = getenv('POSTGRES_DB');
+      $user = getenv('POSTGRES_USER');
+      $pass = getenv('POSTGRES_PASSWORD');
+
+      //conecta ao banco (Data Source Name)
+      $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+
+      try {
+        self::$instance = new PDO($dsn, $user, $pass, [
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+      } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Erro de conexão com o banco de dados.']);
+        exit;
+      }
+    }
+    return self::$instance;
+  }
+}
