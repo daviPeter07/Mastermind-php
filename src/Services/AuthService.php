7dcbfe635beb66ac81911996fe1e44dbe3125d91
daviPeter07 <davipetersondev173@gmail.com>
2025-07-20 20:25:39 -0400

    feat: implementation query register

---
+++ src/Services/AuthService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Core\Database;
+use PDO;
+use Exception;
+
+class AuthService {
+  private PDO $db;
+
+  public function __construct() {
+    $this->db = Database::getConnection();
+  }
+
+  /**
+     * Registra um novo usuário no banco de dados.
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return array
+     * @throws Exception
+     */
+
+     public function register(string $name, string $email, string $password): array {
+      $stmt = $this->db->prepare("SELECT id FROM users WHERE email = ?");
+      $stmt->execute([$email]);
+  
+      //verificação de email existente
+      if ($stmt->fetch()) {
+        throw new Exception("Email já cadastrado");
+      }
+      //encripta a senha
+      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+      $stmt = $this->db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+      $stmt->execute([$name, $email, $hashedPassword]);
+
+      return ["message" => "Usuário cadastrado com sucesso"];
+     }
+}
