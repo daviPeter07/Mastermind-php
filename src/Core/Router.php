11452c9b9a0060d6f8ab756834c5ce43f8ac9f7e
daviPeter07 <davipetersondev173@gmail.com>
2025-07-21 16:55:25 -0400

    feat: add routes for get && post

---
+++ src/Core/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Core;
+
+class Router {
+  private array $routes = [];
+
+  public function post(string $uri, array $action) {
+    $this->routes["POST"][$uri] = $action;
+  }
+
+  public function get(string $uri, array $action) {
+    $this->routes["GET"][$uri] = $action;
+  }
+
+  public function dispacth(){
+    $uri = $_SERVER["REQUEST_URI"];
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if (isset($this->routes[$method][$uri])) {
+      [$controllerClass, $controllerMethod] = $this->routes[$method][$uri];
+      // Cria um novo objeto do Controller (ex: new AuthController())
+      $controller = new $controllerClass();
+      // Chama o método correspondente (ex: $controller->register())
+      $controller->$controllerMethod();
+    } else {
+      http_response_code(404);
+      echo json_encode(["error" => "Endpoint não encontrada"]);
+    }
+  }
+}
