703751aa412c8aaf28a1b083c8b2734648c69207
daviPeter07 <davipetersondev173@gmail.com>
2025-07-20 20:27:56 -0400

    feat: prepare to add routes

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+use App\Services\AuthService;
+
+$requestUrl = $_SERVER['REQUEST_URL'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
 echo "hello world";
