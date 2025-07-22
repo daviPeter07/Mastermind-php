d5cc3aafc059957c2deb1d7ec1782d01b798d4fc
daviPeter07 <davipetersondev173@gmail.com>
2025-07-22 17:01:35 -0400

    feat: prepare backend for login && jwt

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 
+//autoloader do compose
 require __DIR__ . "/../vendor/autoload.php";
 
+//lib pra ler as envs
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
+//import de controller e reqs
 use App\Controllers\AuthController;
 use App\Core\Router;
 
+//res : json
 header('Content-Type: application/json');
 
 $router = new Router();
-
+//rotas e requisições
 $router->post('/api/register', [AuthController::class, 'register']);
-
 $router->dispatch();  
