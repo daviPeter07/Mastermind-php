d4ef43b52f424d6cfbf33be67747f063f7b3eaa5
daviPeter07 <davipetersondev173@gmail.com>
2025-07-24 17:15:01 -0400

    feat: CREATE TABLE categories

--- Database/migration.sql
+++ Database/migration.sql
@@ -1,11 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Cria a nossa tabela de usuários
 CREATE TABLE users (
     -- ID será um UUID, que é um identificador único universal.
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 
-    -- Email deve ser único
     email VARCHAR(255) UNIQUE NOT NULL,
 
     name VARCHAR(255) NOT NULL,
@@ -16,4 +14,25 @@ CREATE TABLE users (
 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY, -- No Postgres, SERIAL é um inteiro que se auto-incrementa.
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL CHECK (type IN ('TASK', 'FINANCE')),
+
+    -- A conexão crucial com a tabela de usuários
+    user_id UUID NOT NULL,
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    -- Define que a coluna user_id é uma chave estrangeira que aponta para o id da tabela users
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)
+        ON DELETE CASCADE, -- Se um usuário for deletado, suas categorias também serão.
+
+    -- Garante que um usuário não pode ter duas categorias com o mesmo nome
+    UNIQUE (user_id, name)
 );
