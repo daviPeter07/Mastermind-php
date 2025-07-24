abe7d466df5e7ad139a019112f0d2a681da096e3
daviPeter07 <davipetersondev173@gmail.com>
2025-07-24 14:12:07 -0400

    feat: add route on index

--- public/index.php
+++ public/index.php
@@ -8,8 +8,9 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 //import de controller e reqs
-use App\Controllers\AuthController;
 use App\Core\Router;
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
 
 //res : json
 header('Content-Type: application/json');
@@ -17,4 +18,5 @@ header('Content-Type: application/json');
 $router = new Router();
 //rotas e requisições
 $router->post('/api/register', [AuthController::class, 'register']);
+$router->get('/api/users', [UserController::class, 'index']);
 $router->dispatch();  
