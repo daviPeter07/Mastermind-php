e064536f691e8b55907ffdccf852aac7b127f88d
daviPeter07 <davipetersondev173@gmail.com>
2025-07-25 20:16:25 -0400

    fix: ident

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -6,80 +6,76 @@ use App\Core\Database;
 use PDO;
 use Exception;
 
-class AuthService {
+class AuthService
+{
   private PDO $db;
   private JwtService $jwtService;
 
-  public function __construct() {
+  public function __construct()
+  {
     $this->db = Database::getConnection();
     $this->jwtService = new JwtService();
   }
 
-     public function register(string $name, string $email, string $password): array {
-      $stmt = $this->db->prepare("SELECT id FROM users WHERE email = ?");
-      $stmt->execute([$email]);
-  
-      //verificação de email existente
-      if ($stmt->fetch()) {
-        throw new Exception("Email já cadastrado");
-      }
-      //encripta a senha
-      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-      $stmt = $this->db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?) RETURNING id, role");
-      $stmt->execute([$name, $email, $hashedPassword]);
-      
-      $newUser = $stmt->fetch(PDO::FETCH_ASSOC);
-      if (!$newUser) {
-        throw new Exception("Falha ao criar usuário");
-      }
+  public function register(string $name, string $email, string $password): array
+  {
+    $stmt = $this->db->prepare("SELECT id FROM users WHERE email = ?");
+    $stmt->execute([$email]);
 
-      $token = $this->jwtService->generateToken($newUser["id"], $newUser["role"]);
+    //verificação de email existente
+    if ($stmt->fetch()) {
+      throw new Exception("Email já cadastrado");
+    }
+    //encripta a senha
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $this->db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?) RETURNING id, role");
+    $stmt->execute([$name, $email, $hashedPassword]);
 
-      $response = [
-        "user" => [
-          "id" => $newUser["id"],
-          "name" => $name,
-          "email" => $email,
-        ],
-        "token" => $token
-      ];
+    $newUser = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$newUser) {
+      throw new Exception("Falha ao criar usuário");
+    }
 
-      return $response;
-     }
+    $token = $this->jwtService->generateToken($newUser["id"], $newUser["role"]);
 
-     /**
-     * Autentica um usuário e retorna um token JWT.
-     * @param string $email
-     * @param string $password
-     * @return array
-     * @throws Exception
-     */
+    $response = [
+      "user" => [
+        "id" => $newUser["id"],
+        "name" => $name,
+        "email" => $email,
+      ],
+      "token" => $token
+    ];
 
-     public function login(string $email, string $password): array {
-      $stmt = $this->db->prepare("SELECT * FROM users WHERE email = ?");
-      $stmt->execute([$email]);
-      $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $response;
+  }
 
-      if (!$user) {
-        throw new Exception("Email ou senha inválidos");
-      }
-      
-      $isPassword = password_verify($password, $user["password"]);
-      if (!$isPassword) {
-        throw new Exception("Email ou senha inválidos");
-      }
+  public function login(string $email, string $password): array
+  {
+    $stmt = $this->db->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-      $token = $this->jwtService->generateToken($user["id"], $user["role"]);
+    if (!$user) {
+      throw new Exception("Email ou senha inválidos");
+    }
 
-      $response = [
-        "token" => $token,
-        'user' => [
-            'id' => $user['id'],
-            'name' => $user['name'],
-            'email' => $user['email'],
-            'role' => $user['role']
-          ]
-      ];
-      return $response;
-     }
+    $isPassword = password_verify($password, $user["password"]);
+    if (!$isPassword) {
+      throw new Exception("Email ou senha inválidos");
+    }
+
+    $token = $this->jwtService->generateToken($user["id"], $user["role"]);
+
+    $response = [
+      "token" => $token,
+      'user' => [
+        'id' => $user['id'],
+        'name' => $user['name'],
+        'email' => $user['email'],
+        'role' => $user['role']
+      ]
+    ];
+    return $response;
+  }
 }
