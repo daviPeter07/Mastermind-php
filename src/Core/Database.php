3dc668faaaa6323da4aad0dd9f56e1550efb3c33
daviPeter07 <davipetersondev173@gmail.com>
2025-08-01 23:53:06 -0400

    feat: sync on render.yaml

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -1,41 +1,26 @@
 <?php
 
 namespace App\Core;
-
 use PDO;
 use PDOException;
 
 class Database
 {
   private static ?PDO $instance = null;
-
   public static function getConnection(): PDO
   {
     if (self::$instance === null) {
-      $connectionUrl = $_ENV['DATABASE_URL'] ?? null;
-
-      if (!$connectionUrl) {
-        die("Erro fatal: A variável de ambiente DATABASE_URL não foi definida.");
-      }
-
-      $dbParts = parse_url($connectionUrl);
-
-      if ($dbParts === false) {
-          die("Erro fatal: A DATABASE_URL está mal formatada.");
-      }
-
-      $host = $dbParts['host'] ?? null;
-      $port = $dbParts['port'] ?? null;
-      $user = $dbParts['user'] ?? null;
-      $pass = $dbParts['pass'] ?? null;
-      $db   = isset($dbParts['path']) ? ltrim($dbParts['path'], '/') : null;
+      $host = getenv("DB_HOST");
+      $port = getenv("DB_PORT");
+      $user = getenv("POSTGRES_USER");
+      $pass = getenv("POSTGRES_PASSWORD");
+      $db   = getenv("POSTGRES_DB");
 
       if (!$host || !$port || !$user || !$pass || !$db) {
-          die("Erro fatal: A DATABASE_URL está incompleta.");
+        die("Erro fatal: Variáveis de ambiente do banco ausentes ou incompletas.");
       }
 
       $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-
       try {
         self::$instance = new PDO($dsn, $user, $pass, [
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
