3310eb5772f0f3e13e5767375b7d266e348b8fa5
daviPeter07 <davipetersondev173@gmail.com>
2025-08-02 00:45:11 -0400

    feat: remove conf

--- start.sh
+++
@@ -1,8 +0,0 @@
-# Espera a DATABASE_URL existir
-while [ -z "${DATABASE_URL}" ]; do
-  echo "Aguardando DATABASE_URL..."
-  sleep 1
-done
-
-echo "Variáveis de ambiente prontas. Iniciando o Supervisor."
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
