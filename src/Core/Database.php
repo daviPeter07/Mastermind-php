1450aa861f0e575fb2af3a10cde221c75ce3c62b
daviPeter07 <davipetersondev173@gmail.com>
2025-08-01 23:48:36 -0400

    fix: alter to  on database

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -12,18 +12,27 @@ class Database
   public static function getConnection(): PDO
   {
     if (self::$instance === null) {
-      $connectionUrl = getenv('DATABASE_URL');
+      $connectionUrl = $_ENV['DATABASE_URL'] ?? null;
 
-      if ($connectionUrl === false) {
+      if (!$connectionUrl) {
         die("Erro fatal: A variável de ambiente DATABASE_URL não foi definida.");
       }
 
       $dbParts = parse_url($connectionUrl);
-      $host = $dbParts['host'];
-      $port = $dbParts['port'];
-      $user = $dbParts['user'];
-      $pass = $dbParts['pass'];
-      $db   = ltrim($dbParts['path'], '/');
+
+      if ($dbParts === false) {
+          die("Erro fatal: A DATABASE_URL está mal formatada.");
+      }
+
+      $host = $dbParts['host'] ?? null;
+      $port = $dbParts['port'] ?? null;
+      $user = $dbParts['user'] ?? null;
+      $pass = $dbParts['pass'] ?? null;
+      $db   = isset($dbParts['path']) ? ltrim($dbParts['path'], '/') : null;
+
+      if (!$host || !$port || !$user || !$pass || !$db) {
+          die("Erro fatal: A DATABASE_URL está incompleta.");
+      }
 
       $dsn = "pgsql:host=$host;port=$port;dbname=$db";
 
